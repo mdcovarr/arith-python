387a7fbbcbca9351f84fd31d8625e38f7096af1a
Michael C <mdcovarrubias@gmail.com>
2020-01-09 15:42:41 -0800

    Add function to validate tokens
    validate_expression is a simple validation that only checks
    that valid characters were passed in the expression

--- src/arith.py
+++ src/arith.py
@@ -15,15 +15,31 @@ class Arith:
         """
         self.expression = ""
         self.tokens = []
+        self.valid_tokens = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '-', '+', '*']
 
     def parse(self):
         """
         Function which takes a string and attempts to return an abstract syntax
         tree if valid input to arith language
         """
-        # first need to parse
+        # 1st validate the expression
+        self.validate_expression()
+
+        # 2st need to parse
         self.tokens = self.expression.split()
 
+    def validate_expression(self):
+        """
+        Function used to validate the expression as valid syntax
+        :return is_valid: boolean that is true if expression is valid, false else
+        """
+        is_valid = False
+        for character in self.expression:
+            if character not in self.valid_tokens:
+                return is_valid
+
+        is_valid = True
+        return is_valid
 
     def run(self):
         """
@@ -34,7 +50,10 @@ class Arith:
         while True:
             try:
                 self.expression = raw_input("Enter expression to be parsed: ")
-                self.parse()
+                if self.validate_expression():
+                    self.parse()
+                else:
+                    print("'{0} is an invalid expression'".format(self.expression))
             except KeyboardInterrupt:
                 print("\nGracefully shutting down...")
                 try:
